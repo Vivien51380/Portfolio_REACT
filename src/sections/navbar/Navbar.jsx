69b60bfe42bdba8b31034e4f7cafed2dd96f84b4
Vivien51380 <mereauxvivien@gmail.com>
2023-08-01 09:43:40 +0200

    create navbar whithout style

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
+import Logo from '../../assets/Logo.jpg';
 import './navbar.css';
+import data from './data';
+import {BiColor} from 'react-icons/bi';
 
 const Navbar = () => {
   return (
-    <nav><h2>navbar</h2></nav>
+    <nav>
+      <div className="container nav__container">
+        <a href="index.html" className='nav__logo'>
+          <img src={Logo} alt="Logo" />
+        </a>
+          <ul className='nav__menu'>
+            {
+              data.map(item => <li key={item.id}><a href={item.linl}>{item.title}</a></li>)
+            }
+          </ul>
+          <button id='theme__icon'><BiColor /></button>
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
---
+++ src/sections/navbar/data.js
@@ -0,0 +1,10 @@
+
+const data = [
+    {id: 1, link: '#', title: 'Accueil'},
+    {id: 2, link: '#about', title: 'À propos'},
+    {id: 3, link: '#services', title: 'Services'},
+    {id: 4, link: '#portfolio', title: 'Portfolio'},
+    {id: 5, link: '#contact', title: 'Contact'},
+]
+
+export default data;
